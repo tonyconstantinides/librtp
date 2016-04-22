3eb559d30fb44d1e47f648d7459cd093cb9bc2a3
tonyconstantinides <tonyconstantinides@gmail.com>
2016-04-21 17:04:17 -0700

    adding catch to project back in

--- src/MjpegManager.cpp
+++ src/MjpegManager.cpp
@@ -28,7 +28,32 @@ MjpegManager::MjpegManager()
     ApiState = ApiStatus::OK;
 }
 
-ApiStatus MjpegManager::connectToIPCam(CamParmsEncription& value)
+ApiStatus MjpegManager::connectToIPCam(CamParmsEncription& camAuth)
 {
+    // decode parms
+    std::string encodedStr;
+    // decode it
+    encodedStr =  camAuth.getCameraGuid();
+    std::string cameraGuid = camAuth.base64_decode(encodedStr);
+    
+    encodedStr = camAuth.getUserName();
+    std::string userName = camAuth.base64_decode(encodedStr);
+    
+    encodedStr = camAuth.getPassword();
+    std::string password = camAuth.base64_decode(encodedStr);
+    
+    encodedStr = camAuth.getHost();
+    std::string  host  = camAuth.base64_decode(encodedStr);
+    
+    encodedStr = camAuth.getPort();
+    std::string  port   = camAuth.base64_decode(encodedStr);
+    
+    encodedStr = camAuth.getAbsPath();
+    std::string  absPath = camAuth.base64_decode(encodedStr);
+    
+    encodedStr =  camAuth.getQueryParms();
+    std::string  queryParms = camAuth.base64_decode(encodedStr);
+    
+    
     return ApiStatus::OK;
 }
