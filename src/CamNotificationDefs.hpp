77d19a4b661f4bd92316858bca4e47ff3564279c
tonyconstantinides <tonyconstantinides@gmail.com>
2016-05-17 17:16:41 -0700

    Camera Defs to be used for Notifications

---
+++ src/CamNotificationDefs.hpp
@@ -0,0 +1,20 @@
+//
+//  CamNotificationDefs.hpp
+//  evartp
+//
+//  Created by Tony Constantinides on 5/17/16.
+//  Copyright © 2016 Bowers & Wilkins. All rights reserved.
+//
+
+#ifndef CamNotificationDefs_h
+#define CamNotificationDefs_h
+
+static const std::string IPCamDiscoveryFailed = "ipcam_discovereryFailedKey";
+static const std::string  IPCamAuthenticationFailed =
+    "ipcam_authenticationFailedKey";
+static const std::string  IPCamStreamStopped =
+    "ipcam_streamStoppedKey";
+static const std::string  IPCamUnrecoverableError =
+    "ipcam_unrecoverableErrorKey";
+
+#endif /* CamNotificationDefs_h */
